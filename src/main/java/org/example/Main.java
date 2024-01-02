dfc20babbf5e047ab2a3a2bfdc278abeaf64297f
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-01-02 17:13:11 -0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,7 @@ package org.example;
 import org.example.DoublyLinkedList.DoublyLinkedList;
 
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
-
 }
